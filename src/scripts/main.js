d182a281d60b3de4aaa4ebd097e92d0da2866f82
jacob thompson <thampsen@gmail.com>
2017-07-28 02:05:24 +0100

    initial commit of dok-7 mockups in-browser prototype

---
+++ src/scripts/main.js
@@ -0,0 +1,30 @@
+var Clipboard = require('clipboard');
+var $ = require('jquery');
+
+$(document).ready(function(){
+	$('.c-modal-trigger').click(function(e){
+    e.preventDefault();
+    $('.c-modal-overlay').show();
+    $('.c-modal-container').addClass('anim-floatUp');
+  });
+  $('.c-modal-close a').click(function(e){
+    e.preventDefault();
+    $('.c-modal-overlay').hide();
+  });
+	$('#referral-url').click(function(e){
+		e.preventDefault();
+	});
+});
+
+new Clipboard('#referral-url', {
+  // called when clip board target '.btn-copy' is clicked
+  // lets you set the actual target to a different element, e.g. the link
+  target: function (trigger) {
+    return trigger;
+  },
+  // called when clip board target '.btn-copy' is clicked
+  // lets you set the text that is put in the clip board
+  text: function (trigger) {
+    return document.getElementById('referral-url').innerHTML;
+  }
+});
